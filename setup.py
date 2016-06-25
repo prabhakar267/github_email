4295223e2d1530e0ba2fcd2756e727beeb43e75e
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-06-25 21:23:55 +0530

    upgrade to v 0.0.2

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@
 # @Author: prabhakar
 # @Date:   2016-06-25 20:13:13
 # @Last Modified by:   Prabhakar Gupta
-# @Last Modified time: 2016-06-25 21:17:30
+# @Last Modified time: 2016-06-25 21:21:06
 
 from setuptools import setup
 
 setup(
     name='github_email',
     packages=['github_email'],
-    version='0.0.1',
+    version='0.0.2',
     description='Get email ID of any GitHub user',
     long_description='Get a list of email IDs of any valid GitHub user from one function call even if there are no public email for that user',
     author='Prabhakar Gupta',
     author_email='prabhakargupta267@gmail.com',
     url='https://github.com/prabhakar267/github_email',
-    download_url='https://github.com/prabhakar267/github_email/tarball/0.0.1',
+    download_url='https://github.com/prabhakar267/github_email/tarball/0.0.2',
     keywords=['github', 'email', 'user', 'public', 'commit', 'get'],
     license='MIT',
     install_requires=['requests'],
