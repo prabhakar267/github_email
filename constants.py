8caa7e8097ba40e3fb630c8ec38eadbb044ca709
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-06-25 15:50:11 +0530

    functionality complete

---
+++ constants.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Author: prabhakar
+# @Date:   2016-06-25 14:48:16
+# @Last Modified by:   Prabhakar Gupta
+# @Last Modified time: 2016-06-25 15:12:23
+
+GITHUB_URL = "https://api.github.com/"
+
+ERROR_DICT = {
+    'Not Found' : "You need to enter a valid GitHub Username",
+    'ValueError' : "You need to enter a valid number",
+    'Limit Exceed' : "You have exceeded the maximum number of tries allowed by free GitHub APIs",
+    'ConnectionError' : "Proper internet connection not found",
+}
--- main_temp.py
+++ main_temp.py
@@ -2,77 +2,93 @@
 # @Author: prabhakar
 # @Date:   2016-06-23 00:22:41
 # @Last Modified by:   Prabhakar Gupta
-# @Last Modified time: 2016-06-24 21:39:03
+# @Last Modified time: 2016-06-25 15:49:49
 
 import requests
 import sys
 import json
 
-GITHUB_URL = "https://api.github.com/"
+from constants import ERROR_DICT, GITHUB_URL
 
 def get_json_response(url):
     response = requests.get(url)
     return json.loads(response.text)
 
+def add_email(email_set, email, max_len):
+    if len(email_set) >= max_len:
+        break_flag = True
+    else:
+        old_set_length = len(email_set)
+        email_set = email_set | set([email])
+
+        if len(email_set) > old_set_length:
+            print "{0} email(s) found".format(len(email_set))
+
+        break_flag = False
+
+    return email_set, break_flag
 
 def main():
     user_email = set([])
-    user = sys.argv[1]
+    try:
+        user = sys.argv[1]
 
-    users_profile_url = GITHUB_URL + "users/{0}".format(user)
-    response = get_json_response(users_profile_url)
-    print response
-    user_name = response['name']
+        if len(sys.argv) > 1:
+            max_email = int(sys.argv[2])
+        else:
+            max_email = 5
+        
+        users_profile_url = GITHUB_URL + "users/{0}".format(user)
+        response = get_json_response(users_profile_url)
 
-    users_repository_url = GITHUB_URL + "users/{0}/repos?type=owner&sort=updated".format(user)
-    response = get_json_response(users_repository_url)
-    
-    for repo in response:
-        if not repo['fork']:
-            users_repository_name = repo['full_name']
-            repos_commit_url = GITHUB_URL + "repos/{0}/commits".format(users_repository_name)
-            commit_reponse = get_json_response(repos_commit_url)
-            
-            break_flag = False
-            for commit in commit_reponse:
-                if (commit['commit']['committer']['name'] == user_name):
-                    if len(user_email) >= 2:
-                        break_flag = True
-                        break
-                    else:
-                        new_email = set([commit['commit']['committer']['email']])
-                        user_email = user_email | new_email
+        # some error encountered
+        if 'message' in response:
+            return response['message']
+        user_name = response['name']
 
-                if (commit['commit']['author']['name'] == user_name):
-                    if len(user_email) >= 2:
-                        break_flag = True
-                        break
-                    else:
-                        new_email = set([commit['commit']['author']['email']])
-                        user_email = user_email | new_email
+        users_repository_url = GITHUB_URL + "users/{0}/repos?type=owner&sort=updated".format(user)
+        response = get_json_response(users_repository_url)
+        
+        for repo in response:
+            if not repo['fork']:
+                users_repository_name = repo['full_name']
+                repos_commit_url = GITHUB_URL + "repos/{0}/commits".format(users_repository_name)
+                commit_reponse = get_json_response(repos_commit_url)
+                
+                break_flag = False
+                possible_positions = ['committer', 'author']
 
-            if break_flag:
-                break
+                for commit in commit_reponse:
+                    for i in possible_positions:
+                        if (commit['commit'][i]['name'] == user_name):
+                            user_email, break_flag = add_email(user_email, commit['commit'][i]['email'], max_email)
 
-    print user_email
+                        if break_flag:
+                            break
 
-            # print repos_commit_url
-            # break
+                if break_flag:
+                    break
+
+        return user_email
+
+    except IndexError:
+        return u'Not Found'
+
+    except ValueError:
+        return u'ValueError'
+
+    except requests.exceptions.ConnectionError:
+        return u'ConnectionError'
 
 
-        # print i['full_name']
-    # print user_name
-
-    # import ipdb; ipdb.set_trace()
-    # print json.dumps(response, indent=4, sort_keys=True)
-    # response_line_list = response.content.splitlines()
-
-    
 if __name__ == "__main__":
-    main()
+    response = main()
+    
+    if type(response) == unicode:
+        print ERROR_DICT[response]
+    else:
+        for email in response:
+            print email
 
-# https://api.github.com/users/prabhakar267
-# https://api.github.com/users/prabhakar267/repos?type=owner&sort=updated
-# https://api.github.com/repos/prabhakar267/github-classifier/commits
 
-# https://gist.github.com/sindresorhus/4512621
+# "API rate limit exceeded for 106.51.31.23. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)"
